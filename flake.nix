f77b1b316136380153396ab8678f0107e1590813
iagocq <18238046+iagocq@users.noreply.github.com>
2021-05-26 09:00:42 -0300

    update flake.{nix,lock}

--- flake.nix
+++ flake.nix
@@ -3,10 +3,7 @@
 
   inputs = {
     iago-nixpkgs.url = "github:iagocq/nixpkgs";
-
-    # https://github.com/NixOS/nixpkgs/issues/124372
-    nixpkgs-master.url = "github:NixOS/nixpkgs/master";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -23,25 +20,12 @@
     overlays = system:
     let
       nixpkgs-config = { inherit system; config.allowUnfree = true; };
-      nixpkgs-master = import inputs.nixpkgs-master nixpkgs-config;
       iago-nixpkgs = import inputs.iago-nixpkgs nixpkgs-config;
     in
     [
       (final: prev: { zsh-f-sy-h = inputs.zsh-f-sy-h; })
       (final: prev: { nnn-src = inputs.nnn-src; })
       (final: prev: { adguardhome = iago-nixpkgs.adguardhome; })
-      # https://github.com/NixOS/nixpkgs/issues/124368
-      # https://github.com/NixOS/nixpkgs/issues/104340#issuecomment-730815509
-      (final: prev: { discord = prev.callPackage (prev.writeText "base.nix" (builtins.replaceStrings [ "\${libPath}" "Path [" ] [ "\${libPath}:$out/opt/\${binaryName}" "Path [ libdrm mesa"] (builtins.readFile "${inputs.nixpkgs}/pkgs/applications/networking/instant-messengers/discord/base.nix"))) rec {
-        pname = "discord";
-        binaryName = "Discord";
-        desktopName = "Discord";
-        version = "0.0.15";
-        src = prev.fetchurl {
-          url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
-          sha256 = "0pn2qczim79hqk2limgh88fsn93sa8wvana74mpdk5n6x5afkvdd";
-        };
-      }; })
     ];
     mkSystem = { host, system, extra-modules ? [], extra ? {}}: nixpkgs.lib.nixosSystem ({
       inherit system;
