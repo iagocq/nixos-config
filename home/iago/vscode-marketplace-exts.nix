15357037f2e7f64502823243ebdaf8fc3201b949
iagocq <18238046+iagocq@users.noreply.github.com>
2021-05-30 10:31:09 -0300

    add vscode extensions

--- home/iago/vscode-marketplace-exts.nix
+++ home/iago/vscode-marketplace-exts.nix
@@ -12,6 +12,18 @@
     sha256 = "0kmmk6bpsdrvbb7dqf0d3annpg41n9g6ljzc1dh0akjzpbchdcwp";
   }
   {
+    name = "nix-env-selector";
+    publisher = "arrterian";
+    version = "1.0.7";
+    sha256 = "0mralimyzhyp4x9q98x3ck64ifbjqdp8cxcami7clvdvkmf8hxhf";
+  }
+  {
+    name = "Nix";
+    publisher = "bbenoist";
+    version = "1.0.1";
+    sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
+  }
+  {
     name = "bracket-pair-colorizer-2";
     publisher = "CoenraadS";
     version = "0.2.0";
@@ -20,8 +32,32 @@
   {
     name = "doxdocgen";
     publisher = "cschlosser";
-    version = "1.2.2";
-    sha256 = "16z3mj758is8x7pqzgk24jl0ninnbiqd9lvgnzyjbzf0v5l04rwg";
+    version = "1.3.2";
+    sha256 = "08dp9bqxsxxqw8pm1sryl14xrvicagqwlxi5cc1vmksjh30dmn1a";
+  }
+  {
+    name = "dendron";
+    publisher = "dendron";
+    version = "0.43.1";
+    sha256 = "0kgcs8y0zhzdz21x0bha6hqlcbpm5jprzb9fwq7mlw7k8x75fl0m";
+  }
+  {
+    name = "dendron-markdown-preview-enhanced";
+    publisher = "dendron";
+    version = "0.10.39";
+    sha256 = "1indlm3j3hfhl1zv0g92qpw0lac3086f1z1wzyx919z6102g789k";
+  }
+  {
+    name = "dendron-markdown-shortcuts";
+    publisher = "dendron";
+    version = "0.12.1";
+    sha256 = "0xnw44xwxy1fhq4wpqcwwzf2x9mvsi14clkky6vdvbig2kbycs1a";
+  }
+  {
+    name = "dendron-paste-image";
+    publisher = "dendron";
+    version = "1.0.4";
+    sha256 = "1p279am2j3ii44mmdwxz5s1pgfa0mv1p0lyh3nkgr492ij2zn1s1";
   }
   {
     name = "vscode-opencl";
@@ -30,10 +66,34 @@
     sha256 = "0a3r2rwxvha8b26ldcsll9bdfp7vgw5sl3mxaq298mxywim8sl5p";
   }
   {
+    name = "latex-workshop";
+    publisher = "James-Yu";
+    version = "8.18.0";
+    sha256 = "0krdkma1fr0x07qlifv1qj2qv2isv72i47by5dmkzfsgz2ha98nr";
+  }
+  {
     name = "vscode-clangd";
     publisher = "llvm-vs-code-extensions";
     version = "0.1.11";
     sha256 = "1l7nx3a2rm7fls06rvmzlgf2bi2db3w4azxk89xjv2wxzxfaf35y";
+  }
+  {
+    name = "rust-analyzer";
+    publisher = "matklad";
+    version = "0.2.612";
+    sha256 = "0a87lnhzpzlni8b33hdskifi0qir88rnmfbzgsy4m5ahs8isw6az";
+  }
+  {
+    name = "python";
+    publisher = "ms-python";
+    version = "2021.5.842923320";
+    sha256 = "183ram995n9dqg7d9g3bn30a1mg7nkkg4knr814f4j9lqzsai22r";
+  }
+  {
+    name = "jupyter";
+    publisher = "ms-toolsai";
+    version = "2021.6.885080120";
+    sha256 = "1vnpqhmfhywj1jmgbkx1db9kcacwl09hggjgddvd55ybnqic5cvj";
   }
   {
     name = "markdown-preview-enhanced";
@@ -48,16 +108,22 @@
     sha256 = "0phffzqv1nmwsgcx6abgzbzw95zc0zlnhsjv2grs5mcsgrghl759";
   }
   {
+    name = "even-better-toml";
+    publisher = "tamasfe";
+    version = "0.12.2";
+    sha256 = "1vz1sxkg24hsn4zfwzjdry4pp1hrc1fp516xpcyvq3ajr1xddlvs";
+  }
+  {
+    name = "clang-format";
+    publisher = "xaver";
+    version = "1.9.0";
+    sha256 = "0bwc4lpcjq1x73kwd6kxr674v3rb0d2cjj65g3r69y7gfs8yzl5b";
+  }
+  {
     name = "linkerscript";
     publisher = "ZixuanWang";
     version = "1.0.1";
     sha256 = "13fvv7g1ignky8yf48xykyhj1mxsrqdy4jn78n9l0d47hfmsc3q6";
-  }
-  {
-    name = "nix-env-selector";
-    publisher = "arrterian";
-    version = "1.0.7";
-    sha256 = "0mralimyzhyp4x9q98x3ck64ifbjqdp8cxcami7clvdvkmf8hxhf";
   }
 ];
 }
--- home/iago/vscode-with-exts.nix
+++ home/iago/vscode-with-exts.nix
@@ -1,18 +1,6 @@
 { pkg, pkgs }:
 
-let
-  extensions = (with pkgs.vscode-extensions; [
-    tamasfe.even-better-toml
-    coenraads.bracket-pair-colorizer-2
-    james-yu.latex-workshop
-    xaver.clang-format
-    matklad.rust-analyzer
-    ms-python.python
-    bbenoist.Nix
-  ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace (import ./vscode-marketplace-exts.nix).extensions;
-in
-(pkgs.vscode-with-extensions.override {
+pkgs.vscode-with-extensions.override {
   vscode = pkg;
-  vscodeExtensions = extensions;
-})
-
+  vscodeExtensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace (import ./vscode-marketplace-exts.nix).extensions;
+}
