6dbcd578e1c0d93d49971294ba850bbe82c21c84
iagocq <18238046+iagocq@users.noreply.github.com>
2021-10-06 16:22:34 -0300

    add missing fs

--- hosts/amogus/hardware-configuration.nix
+++ hosts/amogus/hardware-configuration.nix
@@ -26,8 +26,17 @@
       device = "rpool/local/nix";
       fsType = "zfs";
     };
+    "/home" = {
+      device = "rpool/safe/home";
+      fsType = "zfs";
+    };
+    "/persist" = {
+      device = "rpool/safe/persist";
+      fsType = "zfs";
+    };
     "/boot" = {
       device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
     };
     "/boot/efi" = {
       device = "/dev/disk/by-label/UEFI";
