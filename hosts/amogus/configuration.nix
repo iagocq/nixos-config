1a1fdfa5769810ad29e1f358f47421979a5fd981
iagocq <18238046+iagocq@users.noreply.github.com>
2021-12-03 14:31:24 -0300

    amogus: add gitlab-runner service

--- hosts/amogus/configuration.nix
+++ hosts/amogus/configuration.nix
@@ -14,5 +14,48 @@
 
   services.gitlab-runner = {
     enable = true;
+    services = {
+      # runner for building in docker via host's nix-daemon
+      # nix store will be readable in runner, might be insecure
+      nix = {
+        # File should contain at least these two variables:
+        # `CI_SERVER_URL`
+        # `REGISTRATION_TOKEN`
+        registrationConfigFile = "/persist/secrets/gitlab-runner-registration";
+        dockerImage = "alpine";
+        dockerVolumes = [
+          "/nix/store:/nix/store:ro"
+          "/nix/var/nix/db:/nix/var/nix/db:ro"
+          "/nix/var/nix/daemon-socket:/nix/var/nix/daemon-socket:ro"
+        ];
+        dockerDisableCache = true;
+        preBuildScript = pkgs.writeScript "setup-container" ''
+          mkdir -p -m 0755 /nix/var/log/nix/drvs
+          mkdir -p -m 0755 /nix/var/nix/gcroots
+          mkdir -p -m 0755 /nix/var/nix/profiles
+          mkdir -p -m 0755 /nix/var/nix/temproots
+          mkdir -p -m 0755 /nix/var/nix/userpool
+          mkdir -p -m 1777 /nix/var/nix/gcroots/per-user
+          mkdir -p -m 1777 /nix/var/nix/profiles/per-user
+          mkdir -p -m 0755 /nix/var/nix/profiles/per-user/root
+          mkdir -p -m 0700 "$HOME/.nix-defexpr"
+
+          . ${pkgs.nixUnstable}/etc/profile.d/nix.sh
+
+          ${pkgs.nixUnstable}/bin/nix-env -i ${lib.concatStringsSep " " (with pkgs; [ nixUnstable cacert git openssh ])}
+
+          ${pkgs.nixUnstable}/bin/nix-channel --add https://nixos.org/channels/nixpkgs-unstable
+          ${pkgs.nixUnstable}/bin/nix-channel --update nixpkgs
+        '';
+        environmentVariables = {
+          ENV = "/etc/profile";
+          USER = "root";
+          NIX_REMOTE = "daemon";
+          PATH = "/nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/default/sbin:/bin:/sbin:/usr/bin:/usr/sbin";
+          NIX_SSL_CERT_FILE = "/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt";
+        };
+        tagList = [ "nix" ];
+      };
+    };
   };
 }
