3c1239289cc2575712c9fe31f740dd5f3074dd22
iagocq <18238046+iagocq@users.noreply.github.com>
2022-02-26 19:30:21 -0300

    add pineapple root key

--- users/root/keys.nix
+++ users/root/keys.nix
@@ -3,12 +3,13 @@ rec {
     lasagna = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII4OA5f/3/LBqtwXHqYuR8F0eVFn2/z/k3SO4VTn5uW4";
     pie = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK/QMOTIYs7yS3umoxhQk8yu3kAWN117xxrOoBpO5LcR";
     moldy-lasagna = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINpayUqh0L0IJnHhgaLq5v2880MPcjuIQ/xHAa2eUtjA";
+    pineapple = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDDxt4yEapODaeOhF0cKbKtUW4lJ7ahiEfP166VX0QxD";
   };
 
   trusted = { };
 
   age = with keys; {
     internal = [ lasagna pie ];
-    external = [ lasagna ];
+    external = [ lasagna pineapple ];
   };
 }
